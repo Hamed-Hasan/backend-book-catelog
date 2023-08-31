b1984f14cd4e3f7795f3c506dcd29ad2155068e4
John Doe <hamedhasan422421@gmail.com>
2023-09-01 01:44:24 +0300

    ✨ Define CategoryValidation interface 📝 Specify input structure for category creation and update 🚧 Disable consistent type definition linting for now 🔧 Added category input validation interface 📚 Document category input validation structure 💡 Prepare for category input handling 🛠️ Set up CategoryValidation interface 📦 Initial CategoryValidation definition

---
+++ src/app/modules/Categories/category.interface.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+export interface CategoryValidation {
+    create: {
+      input: {
+        title: string;
+      };
+    };
+    update: {
+      input: {
+        title: string;
+      };
+    };
+  }
