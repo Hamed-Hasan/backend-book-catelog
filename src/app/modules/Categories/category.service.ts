a552c20977e66757f1fe8eeb7ba6e0c40b940a5c
John Doe <hamedhasan422421@gmail.com>
2023-09-01 01:46:26 +0300

    🔧 Added category service functions 📦 Implement CRUD operations for categories 🛠️ Define create, retrieve, update, and delete functions 📊 Utilize Prisma for database interaction 🔖 Organized and structured category service code 💡 Ready to manage categories effectively! 🌟 Enhanced backend with category functionality 🚀 Pushing category service updates

---
+++ src/app/modules/Categories/category.service.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+// Import necessary modules and types
+import prisma from "../../../shared/prisma";
+import { CategoryValidation } from "./category.interface";
+
+// Define the CategoryValidation type
+
+
+// Define your service functions
+const createCategory = async (categoryData: CategoryValidation['create']['input']) => {
+  return prisma.category.create({
+    data: {
+      title: categoryData.title,
+    },
+  });
+};
+
+const getAllCategories = async () => {
+  return prisma.category.findMany();
+};
+
+const getCategoryById = async (categoryId: string) => {
+  return prisma.category.findUnique({
+    where: {
+      id: categoryId,
+    },
+    // include: {
+    //   books: true, // Include related books
+    // },
+  });
+};
+
+const updateCategory = async (categoryId: string, updates: CategoryValidation['update']['input']) => {
+  return prisma.category.update({
+    where: {
+      id: categoryId,
+    },
+    data: {
+      title: updates.title,
+    },
+  });
+};
+
+const deleteCategory = async (categoryId: string) => {
+  return prisma.category.delete({
+    where: {
+      id: categoryId,
+    },
+  });
+};
+
+export const CategoryService = {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+};
