7d62219b6e8b5b125d73f4968058cae8830a753b
John Doe <hamedhasan422421@gmail.com>
2023-09-01 00:37:09 +0300

    ✨ Added user signup route 📝 Set up authentication routes using Express Router 🛂 Implemented request validation middleware 📦 Created controller functions for user management 🔐 Added endpoint for user signup 📄 Defined validation schema for user creation 🌟 Enhanced project with new authentication features 🚀 Ready to handle user registration and authentication!

---
+++ src/app/modules/User/auth.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import {
+  createUser
+} from './controller.user'; // Import controller functions
+import { UserValidation } from './validation.user';
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  validateRequest(UserValidation.createUser),
+  createUser
+);
+
+
+
+export const authRoutes = router;
