e028a05585ff8c1f5289fc2640d791fb2820f23a
John Doe <hamedhasan422421@gmail.com>
2023-09-01 15:17:04 +0300

    ✨ Added authentication routes 🔐 Implemented signup and signin endpoints 🚦 Using Express Router for route organization 🧑‍💻 Integrated AuthController for route handling 📝 Updated authentication routes 🔒 Enhancing security with authentication functionality 🌐 Ready for user signup and signin 💼 Authentication routes are now available

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { AuthController } from './auth.controller';
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  
+  AuthController.signup 
+);
+
+router.post(
+  '/signin',
+  AuthController.signin
+);
+
+export const authRoutes = router;
