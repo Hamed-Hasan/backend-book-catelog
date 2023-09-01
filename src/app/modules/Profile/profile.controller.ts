8ab5f375e8884b9c0ef915e7be5e6d3393544507
John Doe <hamedhasan422421@gmail.com>
2023-09-01 22:06:18 +0300

    ✨ Added getUserProfile route handler 🔒 Implemented user authorization check 🔑 Decoded token to extract userId 📦 Fetched user profile data based on userId ✅ Handled cases for unauthorized and not found profiles 🚀 Improved error handling and response structure 📄 Documented getUserProfile route 🌐 Ready for user profile retrieval!

---
+++ src/app/modules/Profile/profile.controller.ts
@@ -0,0 +1,44 @@
+// Import necessary modules
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { getUserIdFromToken } from '../order/authHelpers';
+import { UserProfileService } from './profile.service';
+
+
+export const UserProfileController = {
+  getUserProfile: async (req: Request, res: Response) => {
+    try {
+      // Extract the token from the request headers
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+      }
+
+      // Decode the token to get the userId
+      const userId = getUserIdFromToken(token as string); // Ensure to handle token type as shown previously
+
+      // Retrieve the user's profile data based on userId
+      const userProfile = await UserProfileService.getUserProfile(userId as string);
+
+      if (!userProfile) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User profile not found');
+      }
+
+      res.status(httpStatus.OK).json({
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'User profile retrieved successfully',
+        data: userProfile,
+      });
+    } catch (error) {
+      res.status(error.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        statusCode: error.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message || 'Internal Server Error',
+        data: null,
+      });
+    }
+  },
+};
