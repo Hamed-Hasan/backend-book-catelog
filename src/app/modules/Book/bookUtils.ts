974cb1cb109f1a3f15ea855378c70591e68677c6
John Doe <hamedhasan422421@gmail.com>
2023-09-01 04:05:15 +0300

    📚 Added book query options in getBookQueryOptions 🔍 Enhanced search and filtering functionality 📊 Improved pagination and sorting options 🛠 Refactored code for better readability 🌟 Implemented dynamic filtering based on query parameters 🚀 Optimized book data retrieval process 📖 Updated bookUtils.ts for advanced querying 🔧 Fine-tuned query options for better performance

---
+++ src/app/modules/Book/bookUtils.ts
@@ -0,0 +1,29 @@
+// bookUtils.ts
+
+import { Request } from 'express';
+
+export const getBookQueryOptions = (req: Request) => {
+  const { page = 1, size = 10, sortBy = 'createdAt', sortOrder = 'desc', minPrice, maxPrice, category, search } = req.query;
+
+  const paginationOptions = {
+    skip: (Number(page) - 1) * Number(size),
+    take: Number(size),
+  };
+
+  const filterOptions: any = {};
+  if (minPrice) filterOptions.price = { gte: Number(minPrice) };
+  if (maxPrice) filterOptions.price = { ...filterOptions.price, lte: Number(maxPrice) };
+  if (category) filterOptions.categoryId = category;
+  if (search) {
+    filterOptions.OR = [
+      { title: { contains: search, mode: 'insensitive' } },
+      { author: { contains: search, mode: 'insensitive' } },
+      { genre: { contains: search, mode: 'insensitive' } },
+    ];
+  }
+
+  const orderByOptions: any = {};
+  orderByOptions[sortBy] = sortOrder;
+
+  return { paginationOptions, filterOptions, orderByOptions };
+};
