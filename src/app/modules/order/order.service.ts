910d45173ae3bea03f9833be657ec97a6d989234
John Doe <hamedhasan422421@gmail.com>
2023-09-01 21:36:11 +0300

    📦 Added OrderService for managing orders 🚀 Implemented createOrder, getAllOrdersForRole, and getOrderById functions 🔒 Secured getOrderById for role-based access 📝 Added documentation comments for better understanding 📊 Utilized Prisma for database interactions 🛠️ Improved error handling with try-catch blocks 🔧 Configured role-based order retrieval logic 📄 Updated order service to match project requirements

---
+++ src/app/modules/order/order.service.ts
@@ -0,0 +1,36 @@
+import { Order } from "@prisma/client";
+import { OrderCreateInput } from "./order.type";
+import prisma from "../../../shared/prisma";
+  
+  export const OrderService = {
+    createOrder: async (orderData: OrderCreateInput): Promise<Order> => {
+      return prisma.order.create({ data: orderData });
+    },
+  
+    getAllOrdersForRole: async (role: string, userId?: string) => {
+        try {
+          if (role === 'admin') {
+            // If the role is admin, return all orders
+            return prisma.order.findMany();
+          } else if (role === 'customer' && userId) {
+            // If the role is customer, return orders specific to the customer
+            return prisma.order.findMany({
+              where: { userId },
+            });
+          } else {
+            throw new Error('Invalid role');
+          }
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+  
+    getOrderById: async (orderId: string): Promise<Order | null> => {
+      return prisma.order.findUnique({
+        where: {
+          id: orderId,
+        },
+      });
+    },
+  };
+  
